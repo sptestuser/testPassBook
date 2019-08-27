37ecfaf3b437db754b814a9a30bff3a697c2ad8e
sptestuser <53940639+sptestuser@users.noreply.github.com>
2019-08-27 14:01:10 +0530

    Update listeners.java

--- src/main/java/com/Technology/listeners.java
+++ src/main/java/com/Technology/listeners.java
@@ -19,7 +19,7 @@ public class listeners implements ITestListener {
 		System.out.println("Listners Failed");
 		baseClass.getScrnShot();	
 		try {
-		 File file= new File("faildata.properties");
+		 File file= new File("faildata.prop");
 	        if(file.exists()){
 	        	file.delete();
 	        }
@@ -39,7 +39,7 @@ public class listeners implements ITestListener {
 
 	public void onStart(ITestContext context) {
 		try {
-			FileOutputStream fos = new FileOutputStream("faildata.properties");
+			FileOutputStream fos = new FileOutputStream("faildata.prop");
 	        fos.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
